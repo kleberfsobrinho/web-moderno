cccd658a58269407b97f79f391fd222788ec2b98
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-02-25 19:25:33 -0300

    add Estruturas de Controle

---
+++ 03-funcao/22-iife.js
@@ -0,0 +1,6 @@
+// IIFE - Imediately Invoked Function Expression
+
+(function () { // foge do escopo global
+    console.log('Será executada na hora!');
+    console.log('Foge do escopo mais abrangente!');
+})()
