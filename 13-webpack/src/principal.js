d5b633b988af35e752e5a7ca9aa3c361c8b4cb21
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-02-28 18:38:33 -0300

    add Webpack

---
+++ 13-webpack/src/principal.js
@@ -0,0 +1,7 @@
+const Pessoa = require('./pessoa')
+// import Pessoa from './pessoa'
+// import './modulos/moduloA'
+import './assets'
+
+const atentende = new Pessoa
+console.log(atentende.cumprimentar())
