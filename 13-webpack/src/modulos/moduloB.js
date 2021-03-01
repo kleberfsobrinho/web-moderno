bdedf2ceb4fc7a6912c7a72c199a9c393791bfb0
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-03-01 09:28:50 -0300

    Revert "add Webpack"
    This reverts commit d5b633b988af35e752e5a7ca9aa3c361c8b4cb21.

--- 13-webpack/src/modulos/moduloB.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    saudacao() {
-        return 'Olá eu sou o Módulo B!!!'
-    }
-}
--- 13-webpack/src/principal.js
+++
@@ -1,7 +0,0 @@
-const Pessoa = require('./pessoa')
-// import Pessoa from './pessoa'
-// import './modulos/moduloA'
-import './assets'
-
-const atentende = new Pessoa
-console.log(atentende.cumprimentar())
