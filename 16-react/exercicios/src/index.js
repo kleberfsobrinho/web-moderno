4c5240f57641f5d727c1ed039906409836ab8133
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-03-22 16:45:30 -0300

    add React

---
+++ 16-react/exercicios/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+//const elemento = <h1>React</h1>
+//import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
+
+ReactDOM.render(<BomDia nome="Kleber" idade={19}/>, document.getElementById('root'))
