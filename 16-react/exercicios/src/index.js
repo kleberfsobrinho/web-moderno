e399e3c40a8ac7cd62404115780d5a064dd8a9d3
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-03-22 17:05:05 -0300

    update Multiplos

--- 16-react/exercicios/src/index.js
+++ 16-react/exercicios/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 //const elemento = <h1>React</h1>
-//import Primeiro from './componentes/Primeiro'
-import BomDia from './componentes/BomDia'
 
-ReactDOM.render(<BomDia nome="Kleber" idade={19}/>, document.getElementById('root'))
+//import Primeiro from './componentes/Primeiro'
+
+//import BomDia from './componentes/BomDia'
+
+import Multi, { BoaNoite } from './componentes/Multiplos'
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Ana" />
+        <BoaNoite nome="Bia" />
+    </div>
+, document.getElementById('root'))
